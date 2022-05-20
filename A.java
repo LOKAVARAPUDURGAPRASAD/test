25f23c60e264aa6986bd0fa1ef9a59f914cd0f98
prasad <lokavarapudurgaprasad1@gmail.com>
2022-05-20 19:36:05 +0530

    ttt

---
+++ A.java
@@ -0,0 +1,14 @@
+   class A
+{
+static int i=100;
+String s="java";
+}
+   class B
+{
+public static void main (String args[])
+{
+System.out.println(A.i);
+A a=new A();
+System.out.println(a.s);
+}
+}
